bcec57a756bcfbeb1fe80c29e83258e964808ff4
UserJul <yuliya.selezneva.1999@gmail.com>
2021-10-15 11:48:18 +0300

    All Home Works

---
+++ HomeWork5/HW5.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HomeWork5
+{
+    public class Class1
+    {
+    }
+}
---
+++ TheAllHomeWorks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheAllHomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
